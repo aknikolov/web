bf985c2483e17f63d34feb2d6528927c99caad1e
aknikolov <60507756+aknikolov@users.noreply.github.com>
2021-06-22 13:33:06 +0300

    Add files via upload

---
+++ web_db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE web_db;
+
+CREATE TABLE conversions (
+  id int(10) NOT NULL AUTO_INCREMENT,
+  userId int(10) NOT NULL,
+  input text NOT NULL,
+  output text NOT NULL,
+  parseType tinyint(1) NOT NULL,
+  timestamp date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE users (
+  username varchar(32) NOT NULL,
+  email varchar(50) NOT NULL,
+  password varchar(32) NOT NULL,
+  id int(10) NOT NULL AUTO_INCREMENT 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
